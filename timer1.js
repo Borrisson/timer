9ec5aeb88763334062b3e88262e15d13bd25eabd
Borrisson <lucborris@gmail.com>
2021-01-12 16:20:03 +0000

    fixed so it takes seconds as arguments instead of milliseconds

--- timer1.js
+++ timer1.js
@@ -1,9 +1,9 @@
-let args = process.argv.slice(2).map(el => Number(el));
+let args = process.argv.slice(2).map(el => Number(el) * 1000);
 const timer = function(args) {
   for (const time of args) {
     if (!isNaN(time) || time >= 0) {
       setTimeout(() => {
-        process.stdout.write(` After ${time} seconds\x07 \n`); // beep didn't work for me added a visual with it though
+        process.stdout.write(` After ${time/1000} seconds\x07 \n`); // beep didn't work for me added a visual with it though
       }, time);
     }
   }
